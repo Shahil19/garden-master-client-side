c567b57a31457707616f98fe41766035fdfd0aed
shahil19 <basharshahil@gmail.com>
2022-05-27 15:03:08 +0600

    navbar and react router added

---
+++ src/Pages/Purchase/Purchase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Purchase = () => {
+    return (
+        <div>
+            <h1 className='text-3xl'>purchase</h1>
+        </div>
+    );
+};
+
+export default Purchase;
